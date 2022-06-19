8fb6a2325212a56d6ef1c3f371d1d8ac1a925b03
Abhi <bhatt23abhishek@gmail.com>
2022-06-19 19:22:19 +0530

    add display images by sub-breed functionality

--- DogImagesBreedWise/script.js
+++ DogImagesBreedWise/script.js
@@ -1,36 +1,69 @@
 var breed='';
-var dogImages;
-var i=0;
+var allBreeds='';
 
-// for filling the dropdown list when the page loads
+// for filling the breed dropdown list when the page loads
 $(document).ready(function(){
     $.get('https://dog.ceo/api/breeds/list/all', function(data){
-        for(let breed in data.message){
+        allBreeds=data.message;
+        console.log(allBreeds);
+        for(let breed in allBreeds){
             let option=`<option value="${breed}">${breed}</option>`;
             $('#dog-breed').append(option);
         }
     });
 });
 
-// get a random image of a dog by breed
+
 $('#get-image-button').click(function(){
-    if($('#dog-breed').val()==breed)
-        return;
+    // remove all the images
+    $('img').remove();
+    // if breed changes remove the sub-breed dropdown
+    if(breed!=$('#dog-breed').val())
+        $('#dog-sub-breed').remove();
+
     breed=$('#dog-breed').val();
-    $.get(`https://dog.ceo/api/breed/${breed}/images`, function(data){
-        dogImages=data.message;
-    })
-    console.log(breed);
-    $.get(`https://dog.ceo/api/breed/${breed}/images/random`, function(data){
-        $('#dog-image').attr('src', data.message);
-    });
+    console.log(allBreeds[breed]);
+
+    let subBreed=$('#dog-sub-breed').val();
+    console.log(subBreed);
+    // if sub-breed dropdown value present display the sub-breed images
+    if(subBreed){
+        $.get(`https://dog.ceo/api/breed/${breed}/${subBreed}/images`, function(data){
+            for(let url of data.message){
+                $('<img>', {
+                    src : url,
+                    height : '20rem',
+                    width : '20rem'
+                }).css('margin', '2rem').appendTo('#dog-images-container');
+            }
+        });
+        return;
+    }
+    // if sub-breed dropdown value not present
+    else{
+        // if sub-breed array of the dog present then display the sub-breed dropdown list
+        if(allBreeds[breed].length>0)
+        {
+            let dropdown=`<select id="dog-sub-breed"></select>`;
+            $('#dog-breed').after(dropdown);
+            for(let subBreed of allBreeds[breed]){
+                $('<option></option>', {
+                    value : subBreed
+                }).text(subBreed).appendTo('#dog-sub-breed');
+            }
+        }
+        // if sub-breed array not present then display the breed images
+        else{
+            $.get(`https://dog.ceo/api/breed/${breed}/images`, function(data){
+                for(let url of data.message){
+                    $('<img>', {
+                        src : url,
+                        height : '20rem',
+                        width : '20rem'
+                    }).css('margin', '2rem').appendTo('#dog-images-container');
+                }
+            });
+        }
+    }
 });
 
-// get the next image of a dog belonging to a particular breed
-$('#next-image-button').click(function(){
-    if(i==dogImages.length)
-        i=0;
-    console.log(dogImages[i]);
-    $('#dog-image').attr('src', dogImages[i]);
-    i++;
-});
